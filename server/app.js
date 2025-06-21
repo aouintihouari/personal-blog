03f86ad9124caffd3ff94775d0811e401f938203
Houari Aouinti <169095690+aouintihouari@users.noreply.github.com>
2025-06-21 15:42:17 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -20,14 +20,24 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 
-const corsOptions = {
-  origin:
-    process.env.NODE_ENV === "production"
-      ? ["https://fullstack-persona-blog.netlify.app"]
-      : ["http://localhost:3000", "http://localhost:5173"],
-  credentials: true,
-};
-app.use(cors(corsOptions));
+const allowedOrigins = [
+  "https://fullstack-persona-blog.netlify.app",
+  "http://localhost:5173",
+  "http://localhost:3000",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
