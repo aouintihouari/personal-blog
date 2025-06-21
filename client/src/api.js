fc0c63fa3620652b61627734da2f1937df784702
Houari Aouinti <169095690+aouintihouari@users.noreply.github.com>
2025-06-21 14:34:15 +0100

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${api}:3000/api/v1`,
+  baseURL: import.meta.env.api,
   headers: {
     "Content-Type": "application/json",
   },
